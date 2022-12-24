763c6031c06e2403ab503f099d1d4cf98b22f50f
TO-HAlawad <78421756+TO-HAlawad@users.noreply.github.com>
2022-12-24 22:17:49 +0000

    Update live to output generated at 31fd5fe

--- index.8210693a.js
+++ index.8210693a.js
@@ -1,2 +1,2 @@
 function myFunction(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("myBtn");"none"===e.style.display?(e.style.display="inline",t.innerHTML="weiterlesen...",n.style.display="none"):(e.style.display="none",t.innerHTML="wenigerlesen...",n.style.display="inline")}
-//# sourceMappingURL=index.23b4f5fd.js.map
+//# sourceMappingURL=index.8210693a.js.map
